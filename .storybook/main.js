907c379056d5ce368f5a35ccc0a0f05529c9849b
Atsushi <niconyan713@gmail.com>
2023-06-02 15:43:13 +0900

    Update storybook to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
-module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ]
-}
+const config = {
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  framework: {
+    name: "@storybook/html-webpack5",
+    options: {}
+  }
+};
+
+export default config;
